66d3e72645c4133fb92e2aed071fff0703921f5d
elignesin <55211352+elignesin@users.noreply.github.com>
2022-11-17 18:26:00 -0500

    Causing errors in CVmaster

--- CVmaster.R
+++ CVmaster.R
@@ -1,20 +1,22 @@
 library(tidyverse)
 
 CVmaster = function(training_data, training_labels, classifier, 
-                    K = 5, loss = "misclassification") {
+                    K = 5, loss = "misclassification", ...) {
+  ags = list(...)
   
   #Building the blocks
-  training_data$block = rep(0, ncol(training_data))
+  training_data$block = rep(0, nrow(training_data))
+  training_labels_block = numeric()
   ims = distinct(training_data %>% select(image))
   snum = 0
   
   for (im in ims){
     xvals = training_data %>%
-      filter(image = im) %>%
+      filter(image == im) %>%
       pull(X) %>%
       unique()
     yvals = training_data %>%
-      filter(image = im) %>%
+      filter(image == im) %>%
       pull(Y) %>%
       unique()
     
@@ -35,11 +37,45 @@ CVmaster = function(training_data, training_labels, classifier,
           mutate(block = ifelse(
             ((image == im) & (X %in% xs) & (Y %in% ys)), 
             snum, block))
+        training_labels_block = c(training_labels_block, 
+                                  rep(snum, 
+                                      nrow(training_data %>%
+                                                   filter((image == im) & 
+                                                            (X %in% xs) & 
+                                                            (Y %in% ys)))))
       }
     }
   }
   
   #Splitting the blocks
   bks = seq.int(from = 1, to = snum, by = 1)
+  folds = createFolds(bks, k = K)
   
+  error = numeric()
+  for (f in folds){
+    print(f)
+    X = training_data %>%
+      filter(block %in% f) %>%
+      select(-c("X", "Y", "image", "block"))
+    y = training_labels[training_labels_block %in% f]
+    
+    trainy = training_labels[!(training_labels_block %in% f)]
+    train = training_data %>%
+        filter(!(block %in% f)) %>%
+        select(-c("X", "Y", "image", "block")) %>%
+        mutate(trainy = trainy)
+    
+    model = do.call(what = classifier,
+                    args = c(formula = reformulate(colnames(train)
+                                                   [-length(colnames(train))], 
+                                                   response = "trainy"),
+                             data = train,
+                             ags))
+    preds = predict(model, X, type = "class")
+    err = 1 - mean(preds == y)
+    error = c(error, err)
+  }
+  e = mean(error)
+  print(paste0("The ", K, "-fold CV-loss is: ", e))
+  return(e)
 }
