b1fd77ffee07ed71925ccf3bf0367afbb6482008
elignesin <55211352+elignesin@users.noreply.github.com>
2022-11-23 14:13:00 -0500

    Fixed CV and block_split functions

--- CVmaster.R
+++ CVmaster.R
@@ -1,50 +1,19 @@
 library(tidyverse)
 
 CVmaster = function(training_data, training_labels, classifier, 
-                    K = 5, loss = "misclassification", ...) {
+                    K = 5, loss = "misclassification", 
+                    split = c("image", "block"), ...) {
   ags = list(...)
   
-  #Building the blocks
-  training_data$block = rep(0, nrow(training_data))
-  training_labels_block = numeric()
-  ims = distinct(training_data %>% select(image)) %>% pull(image)
-  snum = 0
-  
-  for (im in ims){
-    xvals = training_data %>%
-      filter(image == im) %>%
-      pull(X) %>%
-      unique()
-    yvals = training_data %>%
-      filter(image == im) %>%
-      pull(Y) %>%
-      unique()
-    
-    xsplit = split(xvals,
-                   cut(seq_along(xvals),
-                       K,
-                       labels = FALSE))
-    
-    ysplit = split(yvals,
-                   cut(seq_along(yvals),
-                       K,
-                       labels = FALSE))
-
-    for (xs in xsplit){
-      for (ys in ysplit){
-        snum = snum + 1
-        training_data = training_data %>%
-          mutate(block = ifelse(
-            ((image == im) & (X %in% xs) & (Y %in% ys)), 
-            snum, block))
-        training_labels_block = c(training_labels_block, 
-                                  rep(snum, 
-                                      nrow(training_data %>%
-                                                   filter((image == im) & 
-                                                            (X %in% xs) & 
-                                                            (Y %in% ys)))))
-      }
-    }
+  if (split == "block"){
+    output = block_split(training_data, training_labels)
+    training_data = output$training_data
+    labels = output$labels
+    snum = output$snum
+  } else if (split == "image") {
+    output = image_split()
+  } else {
+    stop("Improper split method provided")
   }
   
   #Splitting the blocks
@@ -57,9 +26,9 @@ CVmaster = function(training_data, training_labels, classifier,
     X = training_data %>%
       filter(block %in% f) %>%
       select(!c(X, Y, image, block))
-    y = training_labels[training_labels_block %in% f]
+    y = training_labels[labels %in% f]
     
-    trainy = training_labels[!(training_labels_block %in% f)]
+    trainy = training_labels[!(labels %in% f)]
     train = training_data %>%
         filter(!(block %in% f)) %>%
         select(!c(X, Y, image, block)) %>%
